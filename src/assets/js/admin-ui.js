2aed742e442fa3d8d278cd4ae7582e704d22e3b2
Michael Kent <mkent@spredfast.com>
2016-09-17 10:30:13 -0500

    Updating admin ui to have tabs for both locations and schedules - still not functional

--- src/assets/js/admin-ui.js
+++ src/assets/js/admin-ui.js
@@ -1,38 +1,54 @@
+webix.editors.$popup.datetime = webix.extend({
+  body:{ view:"calendar", icons:true, borderless:true, timepicker: true }
+}, webix.editors.$popup.date);
+webix.editors.datetime = webix.extend({
+  popupType: "datetime"
+}, webix.editors.date);
+
 webix.ui({
+  //*
   view:"tabview",
   header: "VoteATX Admin",
-  id:'tabbar',
+  id:"tabs",
   cells:[
     {
-      id:"locationView",
       header: "Locations",
-      view: "datatable",
-      //autoConfig:true,
-      editable:true,
-      editaction:"dblclick",
-      url: '/voting_location'
-      //*
-      ,
-      columns: [
-        {id:"location_name", header:"Name", width:250, editor:"text"},
-        {id:"address", header:"Address", width: 400, editor:"text"},
-        //{id:"county_name", header:"County", width: 100, editor:"combo"},
-        {id:"schedules", header:"Schedules"}
-      ]
-      //*/
+      body: {
+        id:"locationView",
+        view: "datatable",
+        //autoConfig:true,
+        editable:true,
+        editaction:"dblclick",
+        url: '/voting_location',
+        columns: [
+          {id:"location_name", header:"Name", width:250, editor:"text"},
+          {id:"address", header:"Address", width: 400, editor:"text"},
+          {id:"county_name", header:"County", width: 100, editor:"combo"},
+          {id:"schedules", header:"Schedules"}
+        ]
+      }
     },
     {
-      id:"scheduleView",
       header: "Schedules",
-      view: "datatable",
-      editable:true,
-      editaction:"dblclick",
-      url: '/voting_location',
-      columns: [
-        {id:"location_name", header:"Name", width:250, editor:"text"},
-        {id:"address", header:"Address", width: 400, editor:"text"},
-        {id:"county_name", header:"County", width: 100, editor:"combo"}
-      ]
+      body: {
+        id:"scheduleView",
+        view: "datatable",
+        editable:true,
+        editaction:"dblclick",
+        url: '/schedule',
+        columns: [
+          {id:"name", header:"Name", width:250, editor:"text"},
+          {id:"start", header:"Start", width: 400, editor:"datetime"},
+          {id:"stop", header:"Stop", width: 400, editor: "datetime"},
+        ]
+      }
     }
   ]
 });
+
+$$("tabs").setValue("scheduleView");
+$$("scheduleView").add({
+  name: "name",
+  start: new Date(),
+  stop: new Date()
+})
